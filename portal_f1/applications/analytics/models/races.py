dc91b00a6cefde581625bbf58f803ced68ac98ad
Ferruciolms <145029119+Ferruciolms@users.noreply.github.com>
2024-03-04 16:10:33 -0300

    #0011 - Colunas da tabela races referentes as novas tabelas retiradas

--- portal_f1/applications/analytics/models/races.py
+++ portal_f1/applications/analytics/models/races.py
@@ -11,16 +11,6 @@ class Race(models.Model):
     date = models.DateField(verbose_name="Date", blank=False, null=False)
     time = models.TimeField(verbose_name="Time", blank=False, null=True)
     url = models.CharField(max_length=250, verbose_name="Race Url", blank=False, null=False)
-    fp1_date = models.DateField(verbose_name="FP1 Date", blank=True, null=True)
-    fp1_time = models.TimeField(verbose_name="FP1 Time", blank=True, null=True)
-    fp2_date = models.DateField(verbose_name="FP2 Date", blank=True, null=True)
-    fp2_time = models.TimeField(verbose_name="FP2 Time", blank=True, null=True)
-    fp3_date = models.DateField(verbose_name="FP3 Date", blank=True, null=True)
-    fp3_time = models.TimeField(verbose_name="FP3 Time", blank=True, null=True)
-    quali_date = models.DateField(verbose_name="Quali Date", blank=True, null=True)
-    quali_time = models.TimeField(verbose_name="Quali Time", blank=True, null=True)
-    sprint_date = models.DateField(verbose_name="Sprint Date", blank=True, null=True)
-    sprint_time = models.TimeField(verbose_name="Sprint Time", blank=True, null=True)
     circuit = models.ForeignKey(Circuit, on_delete=models.PROTECT, verbose_name="Circuit ID", blank=False, null=False)
 
     def __str__(self):
